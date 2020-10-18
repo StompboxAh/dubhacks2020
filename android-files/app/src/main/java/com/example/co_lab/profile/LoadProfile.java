0a3c074b348f0599b7b6937127b5e6e4a83bc656
kai-hirose <60566825+kai-hirose@users.noreply.github.com>
2020-10-17 17:38:16 -0700

    Fixes in tying xml to content.

--- android-files/app/src/main/java/com/example/co_lab/profile/LoadProfile.java
+++ android-files/app/src/main/java/com/example/co_lab/profile/LoadProfile.java
@@ -1,6 +1,14 @@
 package com.example.co_lab.profile;
-
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.example.co_lab.R;
+
 
 public class LoadProfile extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
 }
